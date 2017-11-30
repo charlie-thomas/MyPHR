599a32ebd0d53f86daec532fb279443cb12a0b97
Charlie Thomas <charlie.thomas321@gmail.com>
2017-11-30 20:08:37 +0000

    Created medicine details fragment. Closes #33

---
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/MedicineDetails.java
@@ -0,0 +1,44 @@
+package com.csbgroup.myphr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MedicineDetails extends Fragment {
+
+    public MedicineDetails() {
+        // Required empty public constructor
+    }
+
+    public static MedicineDetails newInstance() {
+        MedicineDetails fragment = new MedicineDetails();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_medicine_details, container, false);
+
+        ((MainActivity) getActivity()).setToolbar("Medicine A");
+        setHasOptionsMenu(true);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.edit, menu);
+    }
+
+}
