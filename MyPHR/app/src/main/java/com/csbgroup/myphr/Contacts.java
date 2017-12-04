2009313d292d3f8031c54c7aa12099530ad09e0e
Holly <2092916k@student.gla.ac.uk>
2017-12-04 13:01:04 +0000

    closes #27

--- MyPHR/app/src/main/java/com/csbgroup/myphr/Contacts.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/Contacts.java
@@ -3,8 +3,15 @@ package com.csbgroup.myphr;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class Contacts extends Fragment {
 
@@ -20,7 +27,33 @@ public class Contacts extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        ((MainActivity) getActivity()).setToolbar("My Contacts");
+        setHasOptionsMenu(true);
+
+        List<String> contacts = new ArrayList<String>(){{
+            add("Dr. A"); add("Dr. B"); add("Dr. C"); add("Ms. D"); add("Mr. E");}};
+
+        ArrayAdapter<String> contactsAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.simple_list_item,
+                contacts);
+
+        ListView listView = rootView.findViewById(R.id.contacts_list);
+        listView.setAdapter(contactsAdapter);
+
+        return rootView;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.settings, menu);
+    }
 }
