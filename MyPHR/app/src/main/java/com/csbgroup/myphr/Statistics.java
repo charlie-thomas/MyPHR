52ab793a01c693ac496fbffebdcd694ed5ffa7f1
jb <2185633b@student.gla.ac.uk>
2017-12-05 19:34:11 +0000

    created statistics details fragment, closes #31

--- MyPHR/app/src/main/java/com/csbgroup/myphr/Statistics.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/Statistics.java
@@ -3,8 +3,11 @@ package com.csbgroup.myphr;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,23 +28,50 @@ public class Statistics extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_statistics, container, false);
 
         ((MainActivity) getActivity()).setToolbar("My Statistics");
         setHasOptionsMenu(true);
 
-        List<String> contacts = new ArrayList<String>(){{
-            add("Body Mass Index (BMI)"); add("Blood Pressure"); add("Height Velocity"); add("Weight");}};
+        List<String> statistics = new ArrayList<String>(){{
+            add("Height Velocity");
+            add("Weight");
+            add("BMI");
+            add("Blood Pressure");
+        }};
 
-        ArrayAdapter<String> contactsAdapter = new ArrayAdapter<>(
+        ArrayAdapter<String> statisticsAdapter = new ArrayAdapter<>(
                 getActivity(),
                 R.layout.simple_list_item,
-                contacts);
+                statistics);
 
-        ListView listView = rootView.findViewById(R.id.statistics_list);
-        listView.setAdapter(contactsAdapter);
+        ListView listView = rootView.findViewById(R.id.statistics_list );
+        listView.setAdapter(statisticsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment details = StatisticsDetails.newInstance();
+
+                // Create a bundle to pass the medicine name to the details fragment
+                Bundle bundle = new Bundle();
+                bundle.putString("title", parent.getAdapter().getItem(position).toString());
+                details.setArguments(bundle);
+
+                ((MainActivity) getActivity()).switchFragment(details);
+            }
+        });
 
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.settings, menu);
+    }
 }
---
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/StatisticsDetails.java
@@ -0,0 +1,97 @@
+package com.csbgroup.myphr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by JBizzle on 04/12/2017.
+ */
+
+public class StatisticsDetails extends Fragment {
+
+    LineGraphSeries<DataPoint> series;
+
+    public StatisticsDetails() {
+        // Required empty public constructor
+    }
+
+    public static StatisticsDetails newInstance() {
+        StatisticsDetails fragment = new StatisticsDetails();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_statistics_details, container, false);
+
+        Bundle args = getArguments();
+
+        TextView medTitle = rootView.findViewById(R.id.statistics_title);
+        medTitle.setText(args.getString("title", "Statistics"));
+
+        GraphView graph = rootView.findViewById(R.id.statistics_graph);
+        series = new LineGraphSeries<DataPoint>();
+        int date = 0;
+        double variable = 50;
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i =0 ; i<=30; i++){
+            series.appendData(new DataPoint(date,variable),true,500);
+            list.add("Date:"+Integer.toString(date) + "                         "+ (args.getString("title", "Statistics"))+":"+Double.toString(round(variable,2)));
+            date  = date + 1;
+            Random r = new Random();
+            double randomValue = r.nextDouble() * 2 - 1;
+            randomValue /= 10;
+            variable += randomValue;
+        }
+        graph.addSeries(series);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(30);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        ListView listview = (ListView) rootView.findViewById(R.id.statistics_graph_list);
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        setHasOptionsMenu(true);
+
+        return rootView;
+    }
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.edit, menu);
+    }
+
+}
