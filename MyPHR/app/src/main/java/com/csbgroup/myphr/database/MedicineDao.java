58eb33cc7a7570e62a65bec45912a023d996c9b1
Charlie Thomas <charlie.thomas321@gmail.com>
2017-12-08 14:51:48 +0000

    Create medicine entity and DAO. Closes #65

---
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/database/MedicineDao.java
@@ -0,0 +1,31 @@
+package com.csbgroup.myphr.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface MedicineDao {
+
+    @Query("SELECT * FROM medicineentity")
+    List<MedicineEntity> getAll();
+
+    @Query("SELECT title FROM medicineentity")
+    List<String> getAllTitles();
+
+    @Insert
+    void insertAll(MedicineEntity... medicineEntities);
+
+    @Update
+    void update(MedicineEntity medicineEntity);
+
+    @Delete
+    void delete(MedicineEntity medicineEntity);
+
+    @Query("DELETE FROM medicineentity")
+    void deleteAll();
+}
