2553698008a8f6ac18588d5641ecd3f069744cdb
Holly <2092916k@student.gla.ac.uk>
2017-12-23 16:27:03 +0000

    implemented statisticDetails back button functionality, closes #75

--- MyPHR/app/src/main/java/com/csbgroup/myphr/StatisticsDetails.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/StatisticsDetails.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -93,6 +94,16 @@ public class StatisticsDetails extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.edit, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                ((MainActivity) getActivity()).switchFragment(Statistics.newInstance());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
