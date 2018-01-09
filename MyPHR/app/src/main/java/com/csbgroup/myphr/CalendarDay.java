9f944ae3358fd20b4017368a0f53ae15bdc5210a
Charlie Thomas <charlie.thomas321@gmail.com>
2018-01-09 11:22:43 +0000

    Passing the selected date from the calendar through to the calendar day view fragment. Closes #80

--- MyPHR/app/src/main/java/com/csbgroup/myphr/CalendarDay.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/CalendarDay.java
@@ -3,14 +3,13 @@ package com.csbgroup.myphr;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CalendarDay extends Fragment {
 
 
@@ -18,12 +17,28 @@ public class CalendarDay extends Fragment {
         // Required empty public constructor
     }
 
+    public static CalendarDay newInstance() {
+        CalendarDay fragment = new CalendarDay();
+        return fragment;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar_day, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_calendar_day, container, false);
+
+        // Create a grid with two columns (time and events)
+        RecyclerView recyclerView = rootView.findViewById(R.id.calendar_grid);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        // Get the date passed from the Calendar fragment
+        Bundle args = getArguments();
+        Log.d("Date", args.getInt("day") + "/" + args.getInt("month") + "/" + args.getInt("year"));
+
+        return rootView;
     }
 
 }
