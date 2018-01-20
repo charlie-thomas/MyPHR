573b207c5bf68e9d1cf2c744137b9115763e51ac
Holly <2092916k@student.gla.ac.uk>
2018-01-20 16:37:51 +0000

    add contact dialog linked to fab

--- MyPHR/app/src/main/java/com/csbgroup/myphr/Contacts.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/Contacts.java
@@ -1,7 +1,10 @@
 package com.csbgroup.myphr;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,12 +12,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.support.design.widget.FloatingActionButton;
+import com.csbgroup.myphr.database.AppDatabase;
+import com.csbgroup.myphr.database.StatisticsEntity;
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class Contacts extends Fragment {
+
+    FloatingActionButton fab; //the add contact fab
 
     public Contacts() {
         // Required empty public constructor
@@ -26,7 +35,7 @@ public class Contacts extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
@@ -58,6 +67,11 @@ public class Contacts extends Fragment {
             }
         });
 
+        // fab action for adding contact
+        fab = (android.support.design.widget.FloatingActionButton) rootView.findViewById(R.id.contact_fab);
+        buildDialog(fab);
+
+
         return rootView;
     }
 
@@ -65,4 +79,44 @@ public class Contacts extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
+
+    /**
+     * buildDialog builds the pop-up dialog for adding a new contact
+     * @param fab the floating action button which pulls up the dialog
+     */
+    public void buildDialog(FloatingActionButton fab){
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater inflater = getActivity().getLayoutInflater(); // get inflater
+
+                // build the dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setView(inflater.inflate(R.layout.add_contact_dialog,null));
+
+                // action for confirming add
+                builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        // TODO: database activity
+                    }
+                });
+
+                // action for cancelling activity
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+    }
+
+
 }
