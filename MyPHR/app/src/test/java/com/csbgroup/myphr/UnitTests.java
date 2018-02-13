82248246accc55cbf1f60fbcee9e279d0123ef65
Charlie Thomas <charlie.thomas321@gmail.com>
2018-02-13 21:00:26 +0000

    Added unit tests and started on instrumentation tests

---
+++ MyPHR/app/src/test/java/com/csbgroup/myphr/UnitTests.java
@@ -0,0 +1,20 @@
+package com.csbgroup.myphr;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+
+import static org.junit.Assert.*;
+
+public class UnitTests {
+
+    @Test
+    public void isOtherDayMethodTest() throws ParseException {
+        assertTrue(CalendarDay.isOtherDay("01/01/2000", "03/01/2000"));
+    }
+
+    @Test
+    public void isOtherDayMethodFailTest() throws ParseException {
+        assertFalse(CalendarDay.isOtherDay("01/01/2000", "02/01/2000"));
+    }
+}
