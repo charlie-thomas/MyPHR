68b39c2e5f8c57dc4e8ec93bc818e13bf42503f1
GilesGlasgow <2227780m@student.gla.ac.uk>
2018-02-21 17:15:43 +0000

    Added AlarmReceiver for reminder notifications

---
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.csbgroup.myphr;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.exit(0);
+    }
+}
