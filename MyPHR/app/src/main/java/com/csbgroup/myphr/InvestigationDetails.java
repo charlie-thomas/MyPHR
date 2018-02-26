2603070118524b40dcfe92d37d2a6ece06eb3615
Holly <2092916k@student.gla.ac.uk>
2018-02-26 22:25:33 +0000

    Investigations details format checking, closes #128

--- MyPHR/app/src/main/java/com/csbgroup/myphr/InvestigationDetails.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/InvestigationDetails.java
@@ -4,6 +4,8 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.method.KeyListener;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +19,8 @@ import android.widget.EditText;
 import com.csbgroup.myphr.database.AppDatabase;
 import com.csbgroup.myphr.database.InvestigationsEntity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -36,8 +40,8 @@ public class InvestigationDetails extends Fragment {
     private Drawable titlebackground, datebackground, notesbackground;
 
     // error checking booleans
-    private Boolean validTitle;
-    private Boolean validDate;
+    private Boolean validTitle = true;
+    private Boolean validDate = true;
 
     public InvestigationDetails() {
         // Required empty public constructor
@@ -89,9 +93,9 @@ public class InvestigationDetails extends Fragment {
     }
 
     /**
-     * Fetches a single appointment from the database.
-     * @param uid is the primary key of the appointment to be retrieved
-     * @return the appointment entity
+     * Fetches a single investigation from the database.
+     * @param uid is the primary key of the investigation to be retrieved
+     * @return the investigation entity
      */
     private InvestigationsEntity getInvestigation(final int uid) {
 
@@ -159,6 +163,9 @@ public class InvestigationDetails extends Fragment {
 
         if (this.mode.equals("view")) { // entering edit mode
             editMenu.getItem(0).setIcon(R.drawable.tick);
+
+            // activate error checking
+            errorChecking(title, date);
 
             // show the delete button
             delete.setVisibility(View.VISIBLE);
@@ -228,6 +235,66 @@ public class InvestigationDetails extends Fragment {
     public void disableEditing(EditText field){
         field.setBackground(null);
         field.setKeyListener(null);
+    }
+
+    /**
+     * errorChecking live checks the formatting of fields; errors are highlighted to the user
+     * and saving is disabled until they are corrected.
+     * @param et1 is the investigation title, which cannot be empty
+     * @param et2 is the investigation date, which must be a valid date
+     */
+    public void errorChecking(EditText et1, EditText et2){
+
+        final EditText title = et1;
+        final EditText date = et2;
+
+        // title format checking
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (title.getText().length() != 0){validTitle = true;} // valid title
+                else {validTitle = false; title.setError("Title cannot be empty");} // empty title
+
+                // disable/enable save button following format checks
+                if (validTitle && validDate) {editMenu.getItem(0).setEnabled(true);}
+                else {editMenu.getItem(0).setEnabled(false);}
+            }
+
+            // not needed for our purposes
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override public void afterTextChanged(Editable editable) {}
+        });
+
+        // date error checking
+        date.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                String d = date.getText().toString();
+                if (d.length() != 10) {validDate = false; date.setError("Invalid date (DD/MM/YYYY");} // invalid format
+                else {
+                    try { // valid format
+                        validDate = true;
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                        if (!d.equals(sdf.format(sdf.parse(d)))) { // invalid value
+                            validDate = false;
+                            date.setError("Invalid date (DD/MM/YYYY)");
+                        }
+                        else {validDate = true;} // valid value
+                    } catch (ParseException e) {e.printStackTrace();
+                    }
+                }
+
+                // disable/enable save button following format checks
+                if (validTitle && validDate) {editMenu.getItem(0).setEnabled(true);}
+                else {editMenu.getItem(0).setEnabled(false);}
+            }
+
+            // not needed for our purposes
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override public void afterTextChanged(Editable editable) {}
+        });
     }
 
 }
