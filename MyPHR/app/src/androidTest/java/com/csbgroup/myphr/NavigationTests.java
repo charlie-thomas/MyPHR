fcfba361114caf0768efc86a1c1a396969048f41
Charlie Thomas <charlie.thomas321@gmail.com>
2018-02-27 21:15:54 +0000

    Added tests for tab switching

--- MyPHR/app/src/androidTest/java/com/csbgroup/myphr/NavigationTests.java
+++ MyPHR/app/src/androidTest/java/com/csbgroup/myphr/NavigationTests.java
@@ -1,13 +1,26 @@
 package com.csbgroup.myphr;
 
+import android.support.test.espresso.ViewInteraction;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 public class NavigationTests extends ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -22,6 +35,7 @@ public class NavigationTests extends ActivityInstrumentationTestCase2<MainActivi
         mainActivity = getActivity();
     }
 
+    /* Navigation Bar */
     public void testContactsNavBar() {
         onView(withId(R.id.contacts)).perform(click());
         onView(withText("My Contacts")).check(matches(isDisplayed()));
@@ -45,5 +59,59 @@ public class NavigationTests extends ActivityInstrumentationTestCase2<MainActivi
     public void testAppointmentsNavBar() {
         onView(withId(R.id.appointments)).perform(click());
         onView(withText("My Appointments")).check(matches(isDisplayed()));
+    }
+
+    /* Tab switching */
+    public void testMedicineTab() {
+        onView(withId(R.id.medicine)).perform(click());
+        onView(withText("Growth Hormone")).check(matches(isDisplayed()));
+
+        ViewInteraction tab1 = onView(allOf(
+                childAtPosition(allOf(withId(android.R.id.tabs),
+                        childAtPosition(withClassName(is("android.widget.LinearLayout")), 0)), 1), isDisplayed()));
+        tab1.perform(click());
+        onView(withText(R.string.hospital_address)).check(matches(isDisplayed()));
+
+        ViewInteraction tab0 = onView(allOf(
+                childAtPosition(allOf(withId(android.R.id.tabs),
+                        childAtPosition(withClassName(is("android.widget.LinearLayout")), 0)), 0), isDisplayed()));
+        tab0.perform(click());
+        onView(withText("Growth Hormone")).check(matches(isDisplayed()));
+    }
+
+    public void testAppointmentsTab() {
+        onView(withId(R.id.appointments)).perform(click());
+        onView(withText("Check Up 3")).check(matches(isDisplayed()));
+
+        ViewInteraction tab1 = onView(allOf(
+                childAtPosition(allOf(withId(android.R.id.tabs),
+                        childAtPosition(withClassName(is("android.widget.LinearLayout")), 0)), 1), isDisplayed()));
+        tab1.perform(click());
+        onView(withText("03/01/2018")).check(matches(isDisplayed()));
+
+        ViewInteraction tab0 = onView(allOf(
+                childAtPosition(allOf(withId(android.R.id.tabs),
+                        childAtPosition(withClassName(is("android.widget.LinearLayout")), 0)), 0), isDisplayed()));
+        tab0.perform(click());
+        onView(withText("Check Up 3")).check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
