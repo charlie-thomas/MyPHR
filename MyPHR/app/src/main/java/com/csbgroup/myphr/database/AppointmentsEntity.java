1f9cb57cb1734e2c2997144bb53648798fd51fee
Holly Kirkham-Mowbray <2092916k@student.gla.ac.uk>
2018-03-01 23:54:33 +0000

    Update AppointmentsEntity.java

--- MyPHR/app/src/main/java/com/csbgroup/myphr/database/AppointmentsEntity.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/database/AppointmentsEntity.java
@@ -132,14 +132,6 @@ public class AppointmentsEntity {
         this.reminder_type = reminder_type;
     }
 
-    public boolean isRemind_week(){
-        return remind_week;
-    }
-
-    public void setRemind_week(boolean remind_week){
-        this.remind_week = remind_week;
-    }
-
     public boolean isRemind_week() {
         return remind_week;
     }
@@ -148,27 +140,11 @@ public class AppointmentsEntity {
         this.remind_week = remind_week;
     }
 
-    public boolean isRemind_day() {
-        return remind_day;
-    }
-
-    public void setRemind_day(boolean remind_day) {
-        this.remind_day = remind_day;
-    }
-
     public boolean isRemind_morning() {
         return remind_morning;
     }
 
     public void setRemind_morning(boolean remind_morning) {
-        this.remind_morning = remind_morning;
-    }
-
-    public boolean isRemind_morning(){
-        return remind_morning;
-    }
-
-    public void setRemind_morning(boolean remind_morning){
         this.remind_morning = remind_morning;
     }
 
