fb6047c41c780343fe96c886fc77597c38c7774f
GilesGlasgow <2227780m@student.gla.ac.uk>
2018-03-11 10:40:52 +0000

    Removed unused import statements

--- MyPHR/app/src/main/java/com/csbgroup/myphr/MainActivity.java
+++ MyPHR/app/src/main/java/com/csbgroup/myphr/MainActivity.java
@@ -1,9 +1,6 @@
 package com.csbgroup.myphr;
 
 import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
